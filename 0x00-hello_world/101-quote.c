6e3326ae7f31620f50a24ab46a8880c36dc8a89d
makauj <makauwanyoike@gmail.com>
2024-06-20 13:11:04 +0300

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - Will print last part of quote in case of error
+ * main - Prints out the last part of a quote in stderr
  *
- * Return = 1 if success
+ * Return: 1 (success)
  */
-int main(void)/* Return value of 'main' */
+int main(void)
 {
 	/**
-	 * write requires three arguements
-	 * 1. File descriptor number associated with stderr = 2
-	 * 2. String inside ""
-	 * 3. Total number of characters in the string (59 in this case)
+	 * write function calls three arguements
+	 * 1. the file descriptor for stderr (2)
+	 * 2. the string in quotes
+	 * 3. length of the string (59 in this case)
+	 * Remember to exit special characters
+	 * add \n to add new line
 	 */
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);

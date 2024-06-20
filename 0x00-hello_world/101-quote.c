3f79e57daf1c18050b3b46e1e87538bdcaa18a3c
makauj <makauwanyoike@gmail.com>
2024-06-20 12:52:43 +0300

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,17 +2,18 @@
 #include <unistd.h>
 
 /**
- * main - Code goes here
+ * main - Will print last part of quote in case of error
  *
- * Return = 1
- *
- * File descriptor number associated with sterr = 2
- * 2 is used to identify the stream when a program writes to stderr
- *
- * Add length of the string as the final arguement
+ * Return = 1 if success
  */
-int main(void)
+int main(void)/* Return value of 'main' */
 {
+	/**
+	 * write requires three arguements
+	 * 1. File descriptor number associated with stderr = 2
+	 * 2. String inside ""
+	 * 3. Total number of characters in the string (59 in this case)
+	 */
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

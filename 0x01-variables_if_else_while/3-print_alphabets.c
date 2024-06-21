26d772269d1a4c43a315e870e62441d604b9ace8
makauj <makauwanyoike@gmail.com>
2024-06-21 09:54:14 +0300

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always null (Success)
+ */
+int main(void)
+{
+        int c;
+	int b;
+
+        for (c = 'a'; c <= 'z'; ++c)
+        {
+                putchar(c);
+        }
+	for (b = 'A'; b <= 'Z'; ++b)
+	{
+		putchar(b);
+	}
+        putchar("\n");
+        return (0);
+}

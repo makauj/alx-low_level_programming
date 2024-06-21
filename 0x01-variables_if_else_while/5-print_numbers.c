6b8b3e9f68ed752702b250cf116f1e2b72ca8df5
makauj <makauwanyoike@gmail.com>
2024-06-21 10:20:43 +0300

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always null (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = "1"; c < "10"; c++)
+	{
+		putchar(c);
+	}
+	putchar("\n");
+	return (0);
+}

60be8323e607437d23c7826876641f92bb0a6988
makauj <makauwanyoike@gmail.com>
2024-06-21 14:28:41 +0300

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: returns 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	n = 48;
+
+	while (n <= 57)
+	{
+		putchar (n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar ('\n');
+	return (0);
+}

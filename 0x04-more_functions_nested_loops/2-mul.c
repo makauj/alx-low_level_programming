d037aac0e0289f39829bbf64fbfc89f04f491151
makauj <makauwanyoike@gmail.com>
2024-06-27 10:57:32 +0300

    Collaboration is multiplicatio

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers
+ * @a: check for 'a'
+ * @b: check for 'b'
+ * Return: null (Success)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

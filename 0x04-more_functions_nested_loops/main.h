098023ac1963f42b055ede80e9232875896470b0
makauj <makauwanyoike@gmail.com>
2024-06-28 00:20:25 +0300

    Numbers have life; they are not just symbols on paper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -11,8 +11,10 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+void print_number(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+int b10_pow(int idx);
 
 #endif

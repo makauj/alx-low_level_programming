9e1c5b995eb275ee18e59f340fd76c37e71b217b
makauj <makauwanyoike@gmail.com>
2024-06-27 12:58:23 +0300

    Fail

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - function prints a straight line int the terminal
+ * @n: number of times _ should be printed
+ * Return: null
+ */
+void print_line(int n)
+{
+	int c;
+
+	for (c = 0; c < n; n++)
+		_putchar('_');
+
+	_putchar('\n');
+}

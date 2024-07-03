c09beac68535f005e4153369ce67203c1b441862
makauj <makauwanyoike@gmail.com>
2024-07-03 22:22:18 +0300

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,14 +11,12 @@ int _strcmp(char *s1, char *s2)
 	for ( ; *s1 != '\0' && *s2 != '\0'; s1++, s2++)
 	{
 		if (*s1 != *s2)
-		{
-			return (*s1 != *s2);
-		}
+			return (*s1 - *s2);
 		else if (*s1 == '\0' || *s2 == '\0')
 			break;
 	}
 	if (*s1 == *s2)
 		return (0);
 	else
-		return (*s2 - *s1);
+		return (*s1 - *s2);
 }

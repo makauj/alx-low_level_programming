a03147d4e14e0eeed9912e3239be3feea07920f1
makauj <makauwanyoike@gmail.com>
2024-07-12 13:45:00 +0300

    Message

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * renameMe - main function
+ * @argc: number of command line arguements
+ * @argv: An array containing the program command line arguments
+ * Return: null
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		char *i = argv[0];
+
+		while (*i != '\0')
+		{
+			_putchar(*i);
+			i++;
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stddef.h>
+
+int _putchar(char c);
+int main(int argc, char *argv[]);
+
+#endif

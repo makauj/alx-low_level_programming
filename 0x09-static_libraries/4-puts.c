069b47e05670f7401ab4370deeb25b1411287069
makauj <makauwanyoike@gmail.com>
2024-07-13 19:41:02 +0300

    puts

--- 0x09-static_libraries/4-puts.c
+++ 0x09-static_libraries/4-puts.c
@@ -8,6 +8,6 @@
 int main(void) /* Main: start of the code */
 {
 	/* Exit special character in text */
-	puts("\"Programming is like building a multilingual puzzle");
+	_puts("\"Programming is like building a multilingual puzzle");
 	return (0); /* Return null */
 }

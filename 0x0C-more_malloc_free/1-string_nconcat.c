b86ebf4e3388215941bfa5fcc96b51f1f08cfe9e
makauj <makauwanyoike@gmail.com>
2024-07-18 21:08:50 +0300

    Let the strings join

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,7 +23,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= len2)
 		n = len2;
 	if (len1 + n + 1 < len1)
-		return (NULL)
+		return (NULL);
 	conStr = malloc(len1 + n + 1);
 	if (conStr == NULL)
 		return (NULL);

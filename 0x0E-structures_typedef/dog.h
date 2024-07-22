7384000aef2cc77f0f5c295c529a3f704696dba5
makauj <makauwanyoike@gmail.com>
2024-07-22 18:01:32 +0300

    Dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,10 +2,9 @@
 #define DOG_H
 /**
  * struct dog - structure that stores information
- *
  * @name: pointer to the dog's name
  * @age:dog's age (int)
- * @owner: pointer to the name of the dog's owner 
+ * @owner: pointer to the name of the dog's owner
  */
 struct dog
 {

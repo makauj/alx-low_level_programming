a3e8bba78051115d0a848d29832d43512eae3742
makauj <makauwanyoike@gmail.com>
2024-07-25 07:53:39 +0300

    Again

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -51,7 +51,7 @@ int op_div(int a, int b)
 	if (b == 0)
 	{
 		printf("Error\n");
-		exit(EXIT_FAILURE);
+		exit(100);
 	}
 	return (a / b);
 }
@@ -69,7 +69,7 @@ int op_mod(int a, int b)
 	if (b == 0)
 	{
 		printf("Error\n");
-		exit(EXIT_FAILURE);
+		exit(100);
 	}
 	return (a % b);
 }

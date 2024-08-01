3f3cef1b68683e26d544d02869825ed2d29b69b7
makauj <makauwanyoike@gmail.com>
2024-08-01 23:37:45 +0300

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,49 +10,21 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-	const char *nil_str = "(nil)";
-	unsigned int i;
-	int l, s;
 
 	while (h != NULL)
 	{
-		putchar('[');
-		print_number(h->len);
-		putchar(']');
-		putchar(' ');
-
 		if (h->str == NULL)
 		{
-			for (i = 0; nil_str[i] != '\0'; i++)
-			{
-				l = nil_str[i];
-				putchar(l);
-			}
+			printf("[0] (nil)\n");
 		}
 		else
 		{
-			for (i = 0; i < h->len; i++)
-			{
-				s = h->str[i];
-				putchar(s);
-			}
+			printf("[%u] %s\n", h->len, h->str);
 		}
-		putchar('\n');
-		count++;
-		h = h->next;
-	}
-	return (count);
-}
 
-/**
- * print_number - function to print a number
- * @n: the number to be printed
- */
-void print_number(unsigned int n)
-{
-	if (n / 10)
-	{
-		print_number(n / 10);
+		h = h->next;
+		count++;
 	}
-	putchar((n % 10) + '0');
+
+	return (count);
 }

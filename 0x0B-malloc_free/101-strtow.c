780e645347814c4871709b0c4bed8872a356ee86
makauj <makauwanyoike@gmail.com>
2024-08-01 13:31:29 +0300

    merge

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * strtow - function splits strings into words
+ * @str - string to split into words
+ *
+ * Return: split words else NULL
+ */
+char **strtow(char *str)
+{
+
+}

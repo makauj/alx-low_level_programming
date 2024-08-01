9a701336ea4b7daafbd6f2658de4def3a8f6ac56
makauj <makauwanyoike@gmail.com>
2024-08-01 21:01:31 +0300

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,21 +10,19 @@
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
-	char buffer[16];
-	int len = h->len;
 
 	while (h != NULL)
 	{
+		write(STDOUT_FILENO, "[", 1);
+		print_number(h->len);
+		write(STDOUT_FILENO, "] ", 2);
+
 		if (h->str == NULL)
 		{
-			write(STDOUT_FILENO, "[0] (nil)")
+			write(STDOUT_FILENO, "[0] (nil)\n", 10);
 		}
 		else
 		{
-			itoa(h->len, buffer, 10);
-			write(STDOUT_FILENO, "[", 1);
-			write(STDOUT_FILENO, buffer, strlen(buffer));
-			write(STDOUT_FILENO, "] ", 2);
 			write(STDOUT_FILENO, h->str, h->len);
 			write(STDOUT_FILENO, "\n", 1);
 		}
@@ -32,6 +30,22 @@ size_t print_list(const list_t *h)
 		h = h->next;
 	}
 	return (count);
+}
+
+/**
+ * print_number - function to print an unsigned integer
+ * @n: the number to be printed
+ */
+void print_number(unsigned int n)
+{
+	char digit;
+
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	digit = (n % 10) + '0';
+	write(STDOUT_FILENO, &digit, 1);
 }
 
 /**
@@ -48,7 +62,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (new_node == NULL)
 	{
-		write(STDERR_FILENO, "Failed to allocate memory for new node.\n");
+		write(STDERR_FILENO, "Failed to allocate memory for new node.\n", 41);
 		exit(1);
 	}
 
@@ -56,7 +70,7 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node->str == NULL)
 	{
 		free(new_node);
-		write(STDERR_FILENO, "Failed to allocate memory for new node string.\n");
+		write(STDERR_FILENO, "Failed to allocate memory for new node string.\n", 48);
 		exit(1);
 	}
 
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stddef.h>
+#include <unistd.h>
 
 /**
  * struct list_s - singly linked list
@@ -22,6 +23,7 @@ typedef struct list_s {
 }list_t;
 
 size_t print_list(const list_t *h);
+void print_number(unsigned int n);
 list_t *add_node(list_t **head, const char *str);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);

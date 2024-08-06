703d95c5ead219a36091fd560199d68c624d82cc
makauj <makauwanyoike@gmail.com>
2024-08-06 16:03:10 +0300

    print list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,14 +8,13 @@
  */
 size_t listint_len(const listint_t *h)
 {
-	int count = 0;
-	const listint_t *current = h;
+	size_t count = 0;
+	const listint_t *current = *h;
 
 	while (current != NULL)
 	{
 		count++;
 		current = current->next;
 	}
-
 	return (count);
 }

f7bd6e3918d1349432c0c097020b056e857215d1
makauj <makauwanyoike@gmail.com>
2024-08-08 13:00:42 +0300

    Binary to decimal

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -20,6 +20,10 @@ unsigned int binary_to_uint(const char *b)
 	len = strlen(b);
 	for (i = len - 1; i >= 0; i--)
 	{
+		if (b[i] != '0'&& b[i] != '1')
+		{
+			return (0);
+		}
 		if (b[i] == '1')
 		{
 			dec += rem;

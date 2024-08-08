811d892ffc61cbd5399cd125a1ff0474a72cf3cc
makauj <makauwanyoike@gmail.com>
2024-08-08 19:03:44 +0300

    Flip bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,10 +13,10 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int result = n ^ m;
 	unsigned long int count = 0;
 
-	while (result != 0)
+	while (result != 0) /*Loop as long as there are bits in result*/
 	{
-		result &= (result - 1);
-		count += 1;
+		result &= (result - 1); /* Clear the lowest set bit */
+		count += 1; /*Increment the count*/
 	}
 
 	return (count);

8f12c32d1fa73a77d7d22932d4c1f5a3a2191bce
makauj <makauwanyoike@gmail.com>
2024-08-08 18:59:02 +0300

    Flip bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,7 +13,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int result = n ^ m;
 	unsigned long int count = 0;
 
-	for (count = 0; count != 0; count++)
+	for (count = 0; count != 0; count += 1)
 	{
 		result &= (result - 1);
 	}

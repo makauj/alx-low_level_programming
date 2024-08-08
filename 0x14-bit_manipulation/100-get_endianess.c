01e6ec9fd2255faef8894dc9b0c743d2df037d6f
makauj <makauwanyoike@gmail.com>
2024-08-08 19:18:36 +0300

    Endianness

---
+++ 0x14-bit_manipulation/100-get_endianess.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * get_endianness - function that checks the endianess
+ *
+ * Return: 0 if big, 1 if little
+ */
+
+int get_endianness(void)
+{
+	unsigned int x = 1;
+	char *c = (char*) &x;
+
+	if (*c == 1)
+	{
+		return (1); /* Little endian */
+	}
+	else
+	{
+		return (0); /* Big endian */
+	}
+}

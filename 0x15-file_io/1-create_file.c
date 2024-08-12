68f98d0eea80cc7cfd73c8dcb640ad8670413076
Makau <makauwanyoike@gmail.com>
2024-08-13 00:13:08 +0300

    copy contents

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,26 +21,22 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (text_content == NULL)
-	{
-		text_content = "";
-	}
-
 	if (fd == -1)
 	{
 		return (-1);
 	}
-	while (text_content[i] != '\0')
+	if (text_content)
 	{
+		while (text_content[i])
+			i++;
+		
 		f_write = write(fd, text_content, i);
 		if (f_write == -1)
 		{
 			close(fd);
 			return (-1);
 		}
-		i++;
 	}
-
 	close(fd);
 	return (1);
 }

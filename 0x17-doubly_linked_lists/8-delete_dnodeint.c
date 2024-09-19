f2809c3d1c8bb2be9545cd99476557aed00f7b97
makau <makauwanyoike@gmail.com>
2024-09-19 22:36:30 +0300

    delete new node at idx

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes the node at a given index
+ * @head: double pointer to the head of the list
+ * @index: index of the node to be deleted
+ *
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current = *head;
+	unsigned int i;
+
+	if (!current)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = current->next;
+		if (*head) 
+			(*head)->prev = NULL;
+		free(current);
+		return (1);
+	}
+
+	for (i = 0; current != NULL && i < index; i++)
+	{
+		current = current->next;
+	}
+
+	if (current == NULL)
+		return (-1);
+
+	if (current->prev)
+		current->prev->next = current->next;
+
+	if (current->next)
+		current->next->prev = current->prev;
+
+	free(current);
+
+	return (1);
+}

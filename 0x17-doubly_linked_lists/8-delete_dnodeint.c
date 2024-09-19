5705f816bc46efcc79483856ecd3d08b2ca57593
makau <makauwanyoike@gmail.com>
2024-09-20 00:37:41 +0300

    try

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -22,7 +22,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			(*head)->prev = NULL;
 		free(current);
 		return (1);
-    }
+	}
 	for (i = 0; current != NULL && i < index; i++)
 	{
 		current = current->next;

c42a408398e2b38e32a5171998aa9b6f40264a5d
makau <makauwanyoike@gmail.com>
2024-09-19 22:15:43 +0300

    add new node at idx

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,41 +11,47 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-	unsigned int i = 0;
-	dlistint_t *current = *h;
-
-	new_node->n = n;
-	new_node->next = NULL;
-	new_node->prev = NULL;
+	dlistint_t *new_node = NULL;
+	unsigned int i;
+	dlistint_t *current;
 
 	if (idx == 0)
 	{
-		new_node->next = h;
-
-		if (*h)
-			(*h)->prev = new_node;
-
-		*h = new_node;
-		return (new_node);
+		new_node = add_dnodeint;
 	}
 
-	for (i = 1; i < idx - 1 && current != NULL; i++)
-		current = current->next;
-
-	if (!current)
+	else
 	{
-		free(new_node);
-		return (NULL);
+		current = *h;
+		i = 1;
+		if (current)
+		{
+			while (current-> !=NULL)
+				current = current->prev;
+		}
+		while (current)
+		{
+			if (i == idx)
+			{
+				if (current->next == NULL)
+					new_node = add_dnodeint_end(h, n);
+				else
+				{
+					new_node = malloc(sizeof(dlistint_t));
+					if (new_node)
+					{
+						new_node->n = n;
+						new_node->next = current->next;
+						new_node->prev = current;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
+		}
 	}
-
-	new_node-next = current->next;
-	new_node->prev = current;
-
-	if (current->next)
-		current->next->prev = new_node;
-
-	current->next = new_node;
-
 	return (new_node);
 }

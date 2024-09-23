bed2752eaa86c4f42f904b8381d1937df0371413
makau <makauwanyoike@gmail.com>
2024-09-23 18:18:31 +0300

    README

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp https://github.com/makauj/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libhack.so
-export LD_PRELOAD=/tmp/libhack.so
+wget -P .. https://raw.githubusercontent.com/makauj/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
+export LD_PRELOAD="$PWD/../libhack.so"

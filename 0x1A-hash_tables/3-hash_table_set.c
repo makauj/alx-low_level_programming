fb200e5cf3b6f336783e1af48d4179fef7df4c58
makau <makauwanyoike@gmail.com>
2024-10-11 22:56:21 +0300

    betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -43,9 +43,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int idx;
 	hash_node_t *tmp, *node;
 
-	if (!ht ||!key || !value || strlen(key) == 0)
+	if (!ht || !key || !value || strlen(key) == 0)
 		return (0);
-	
 
 	idx = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[idx];

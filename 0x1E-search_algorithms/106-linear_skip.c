b08ffb2d8976bab347175f6bf6c431d21b7bab26
makau <makauwanyoike@gmail.com>
2025-02-01 11:08:55 +0300

    linear skip

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -5,40 +5,42 @@
  * @list: pointer to the head of the skip list
  * @value: value to search for
  *
- * Return: pointer to the first node where value is located, or NULL if not found
+ * Return: pointer to the first node where value is, or NULL if not found
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-    skiplist_t *curr, *prev;
+	skiplist_t *curr, *prev;
 
-    if (list == NULL)
-        return (NULL);
+	if (list == NULL)
+		return (NULL);
 
-    curr = list;
-    prev = list;
+	curr = list;
+	prev = list;
 
-    while (curr->express && curr->n < value)
-    {
-        prev = curr;
-        curr = curr->express;
-        printf("Value checked at index [%lu] = [%d]\n", curr->index, curr->n);
-    }
-    if (curr->express == NULL && curr->n < value)
-    {
-        prev = curr;
-        while (curr->next)
-            curr = curr->next;
-    }
-    printf("Value found between indexes [%lu] and [%lu]\n",
-           prev->index, curr->index);
+	while (curr->express && curr->n < value)
+	{
+		prev = curr;
+		curr = curr->express;
+		printf("Value checked at index [%lu] = [%d]\n",
+				curr->index, curr->n);
+	}
+	if (curr->express == NULL && curr->n < value)
+	{
+		prev = curr;
+		while (curr->next)
+			curr = curr->next;
+	}
+	printf("Value found between indexes [%lu] and [%lu]\n",
+			prev->index, curr->index);
 
-    curr = prev;
-    while (curr && curr->n <= value)
-    {
-        printf("Value checked at index [%lu] = [%d]\n", curr->index, curr->n);
-        if (curr->n == value)
-            return (curr);
-        curr = curr->next;
-    }
-    return (NULL);
+	curr = prev;
+	while (curr && curr->n <= value)
+	{
+		printf("Value checked at index [%lu] = [%d]\n",
+				curr->index, curr->n);
+		if (curr->n == value)
+			return (curr);
+		curr = curr->next;
+	}
+	return (NULL);
 }
